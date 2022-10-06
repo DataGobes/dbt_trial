bf8d2b0cda098d0d8d5809619eb95a1df33ac2af
Gijs Jansen <gijs.jansen@miele.com>
2022-10-06 13:52:35 +0200

    removed files from local

---
+++ docs/compiled/dbt_trial/models/staging/jaffle_shop/stg_jaffle_shop.yml/not_null_stg_customers_customer_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select customer_id
+from main.dbt.stg_customers
+where customer_id is null
+
+
---
+++ docs/compiled/dbt_trial/models/staging/jaffle_shop/stg_jaffle_shop.yml/not_null_stg_orders_order_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select order_id
+from main.dbt.stg_orders
+where order_id is null
+
+
